c4eb57a8261b3f21479b12efe953a902dda89d22
mikesmithlab <mikesmithlab@gmail.com>
2024-08-22 14:20:40 +0100

    added licence etc

---
+++ README.md
@@ -0,0 +1,11 @@
+filehandling
+Repository for setting permissions and distributing onedrive folders
+
+github installation
+To install run the following line in your environment
+
+pip install git+https://github.com/MikeSmithLab/onedrive
+github update
+To update run the following line in your environment
+
+pip install --upgrade git+https://github.com/MikeSmithLab/onedrive
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r") as fh:
+    license = fh.read()
+
+setuptools.setup(
+    name='onedrive',
+    version='0.1',
+    license=license,
+    packages=setuptools.find_packages(
+        exclude=('tests', 'docs')
+    ),
+    url='https://github.com/MikeSmithLab/onedrive',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
